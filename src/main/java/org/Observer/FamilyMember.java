e2421c43e55c0f8ce245c0feef973caf2c2b3005
AndromedaK2 <102329976+AndromedaK2@users.noreply.github.com>
2022-06-09 21:54:17 -0400

    [add] casting

--- src/main/java/org/Observer/FamilyMember.java
+++ src/main/java/org/Observer/FamilyMember.java
@@ -2,14 +2,15 @@
 package org.Observer;
 
 import org.Observable.Dog;
+import org.Observable.IPet;
 
 public class FamilyMember implements IFamilyMember {
 
     private Dog pet;
     public String name;
 
-    public FamilyMember(Dog _pet, String _name) {
-        pet = _pet;
+    public FamilyMember(IPet _pet, String _name) {
+        pet = (Dog) _pet;
         name = _name;
     }
 
